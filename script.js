15373230cb54c13fd0083df97799e22570295a48
HarshiniPaluri68 <231fa04g68@gmail.com>
2025-08-04 18:53:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById("flightForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+  alert("✅ Flight booked successfully!");
+});
+
+document.getElementById("hotelForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+  alert("✅ Hotel booked successfully!");
+});
+
+document.getElementById("guideForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+  alert("✅ Guide booked successfully!");
+});
